68654a18c171ddb161cabd0f1f75625418865c83
zhoujie <1123095177@qq.com>
2024-10-29 10:17:05 +0800

    feat(仪表盘): 订单统计

--- mall-admin-web/src/views/home/index.vue
+++ mall-admin-web/src/views/home/index.vue
@@ -23,14 +23,14 @@
             <div class="total-value">￥5000.00</div>
           </div>
         </el-col>
-        <!--<el-col :span="6">-->
-          <!--<div class="total-frame">-->
-            <!--<svg-icon icon-class="total-week" class="total-icon">-->
-            <!--</svg-icon>-->
-            <!--<div class="total-title">近7天销售总额</div>-->
-            <!--<div class="total-value">￥50000.00</div>-->
-          <!--</div>-->
-        <!--</el-col>-->
+        <el-col :span="6">
+          <div class="total-frame">
+            <svg-icon icon-class="total-week" class="total-icon">
+            </svg-icon>
+            <div class="total-title">近7天销售总额</div>
+            <div class="total-value">￥50000.00</div>
+          </div>
+        </el-col>
       </el-row>
     </div>
     <div class="un-handle-layout">
